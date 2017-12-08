06b2d9b9e836824902674ab212463a56509e31cf
Sunghwan Park <shoark7@gmail.com>
2017-12-08 23:05:00 +0900

    Binary search v1.0.0

--- search-algorithm/binary_search.py
+++ search-algorithm/binary_search.py
@@ -1,9 +1,37 @@
+"""Binary search module
+
+    Start Date: 2017/12/08
+    Last modified Date: 2017/12/08
+
+    github.com/shoark7
+"""
+__version__ = '1.0.0'
+
+
 def binary_search(value, target, reverse=False, changed=False):
+    """Binary search in Python
+
+    This code is designed to be scalable, considering this kind of things,
+        1. ascending or descending can be chosen.
+        2. target list to be ransacked can be sorted during the search.
+        3. And more...
+
+    :input:
+        value:   Value to be searched
+        target:  List or something to be ransacked.
+        reverse: Whether search to be done in descending or not.
+        changed: Whether target to be changed during searching or not.
+
+    :return:
+        Index of the value in target.
+        If value is not in target, returns -1.
+    """
     if value not in target:
         return -1
 
     if changed:
         target.sort(reverse=reverse)
+        ordered = target
     else:
         ordered = sorted(target, reverse=reverse)
 
