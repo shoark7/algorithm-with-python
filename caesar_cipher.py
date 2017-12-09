7a819a0866183033475d115c09e5dbf5201c52cd
Sunghwan Park <shoark7@gmail.com>
2017-12-09 19:17:50 +0900

    Fix indentation issue
    Uploading from Windows sucks.
    Indentation broke out so I fixed it.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,15 +5,15 @@ SKIP = string.digits + string.whitespace + string.punctuation
 
 
 def caesar_cipher_encode(plain_text: str, step=3):
-	cipher = ''
+    cipher = ''
 
-	for c in plain_text:
-		if c in SKIP:
-			cipher += c
-			continue
-		diff = ord(c) - (ord('a') if c in LOWER else ord('A'))
-		diff = (diff + step) % 26
-		cipher += chr(diff + 
-						(ord('a') if c in LOWER else ord('A'))
-					)
-		return cipher
+    for c in plain_text:
+        if c in SKIP:
+            cipher += c
+            continue
+        diff = ord(c) - (ord('a') if c in LOWER else ord('A'))
+        diff = (diff + step) % 26
+        cipher += chr(diff + 
+        	(ord('a') if c in LOWER else ord('A'))
+        )
+    return cipher
