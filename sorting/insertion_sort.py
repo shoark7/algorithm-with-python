06335f43cbe5296fb361abc0f3347cbf2809e525
Sunghwan Park <shoark7@gmail.com>
2018-02-10 09:29:40 +0900

    Create insertion sort in Python

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,19 @@
+"""Insertion sort in Python
+
+
+Date: 2018/02/10
+"""
+
+
+def insertion_sort(target, reverse=False):
+    """Insertion sort in Python"""
+    for i in range(len(target)):
+        tmp = target[i]
+        for j in range(i-1, -1, -1):
+            if tmp < target[j]:
+                target[j+1] = target[j]
+                target[j] = tmp
+            else:
+                break
+
+    return target
