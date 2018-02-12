20fd172585be03eada12c42a0ebc75a3bf2c2bce
Sunghwan Park <shoark7@gmail.com>
2018-02-12 14:21:27 +0900

    Make reverse sort possible

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -7,13 +7,23 @@ Date: 2018/02/10
 
 def insertion_sort(target, reverse=False):
     """Insertion sort in Python"""
-    for i in range(len(target)):
-        tmp = target[i]
-        for j in range(i-1, -1, -1):
-            if tmp < target[j]:
-                target[j+1] = target[j]
-                target[j] = tmp
-            else:
-                break
+    if reverse:
+        for i in range(len(target)):
+            tmp = target[i]
+            for j in range(i-1, -1, -1):
+                if tmp > target[j]:
+                    target[j+1] = target[j]
+                    target[j] = tmp
+                else:
+                    break
+    else:
+        for i in range(len(target)):
+            tmp = target[i]
+            for j in range(i-1, -1, -1):
+                if tmp < target[j]:
+                    target[j+1] = target[j]
+                    target[j] = tmp
+                else:
+                    break
 
     return target
