93196f5e3cc70ab0b4a87fc1c5d07fa462eac494
Sunghwan Park <shoark7@gmail.com>
2018-03-30 16:36:07 +0900

    Fix error in gcd

--- etc_examples/gcd.py
+++ etc_examples/gcd.py
@@ -1,10 +1,9 @@
 """Euclidean algorithm of 2 integers
 
 
-Date : 2018/03/29
+Date : 2018/03/30
 """
 
 
 def gcd(a, b):
-    s, b = min(a, b), max(a, b)
-    return b if s == 0 else gcd(s, b % s)
+    return a if b == 0 else gcd(b, a % b)
