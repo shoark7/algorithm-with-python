26c45b1b17c832afc3e818b32407b95879f71f44
Sunghwan Park <shoark7@gmail.com>
2018-09-07 17:35:39 +0900

    Refactor rock_festival.py

--- problems_solving/algospot/rock_festival.py
+++ problems_solving/algospot/rock_festival.py
@@ -6,12 +6,10 @@ https://algospot.com/judge/problem/read/FESTIVAL
 """
 C = int(input())
 min_lists = []
+MAX_N = 1000
 
-for _ in range(C):
-    N, L = (int(x) for x in input().split())
-    min_ave = 1001
-    costs = [int(x) for x in input().split()]
-
+def get_min_avg(N, L, costs):
+    min_ave = MAX_N + 1
     for l in range(L, N+1):
         tmp = sum(costs[:l]) / l
         min_ave = min(min_ave, tmp)
@@ -19,8 +17,12 @@ for _ in range(C):
             tmp = tmp - costs[i] / l + costs[i+l] / l
             min_ave = min(tmp, min_ave)
 
+    return min_ave
+
+
+for _ in range(C):
+    N, L = (int(x) for x in input().split())
+    costs = [int(x) for x in input().split()]
+    min_avg = get_min_avg(N, L, costs)
+
     min_lists.append(min_ave)
-
-
-for i in range(C):
-    print(min_lists[i])
