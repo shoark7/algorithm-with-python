2e483f336876048b5ef9d7257ab903120543f593
Sunghwan Park <shoark7@gmail.com>
2018-10-24 10:59:59 +0900

    Update Longest Increasing Subsequence problem

--- problems_solving/algospot/longest_increasing_subsequence.py
+++ problems_solving/algospot/longest_increasing_subsequence.py
@@ -38,13 +38,50 @@ def lis(arr):
     return search(0) - 1
 
 
+# Get the lis itself, not the length of it(Actually we need this, aren't we?)
+def get_real_lis(arr):
+    arr = [-1] + arr
+    length = len(arr)
+    cache = [-1 for _ in range(length)]
+    choices = [-1 for _ in range(500)]
+    ans = []
+
+    def find(start):
+        if cache[start] != -1:
+            return 1
+        ret = 1
+        best_next = -1
+
+        for i in range(start+1, length):
+            if arr[start] < arr[i]:
+                cand = find(i) + 1
+                if cand > ret:
+                    ret = cand
+                    best_next = i
+        choices[start+1] = best_next
+        return ret
+
+    def reconstruct(start, ans_arr):
+        if choices[start] != -1:
+            ans_arr.append(arr[start])
+        nxt = choices[start+1]
+        if nxt != -1:
+            reconstruct(nxt, ans_arr)
+
+    find(0)
+    reconstruct(0, ans)
+    return ans
+
+
 if __name__ == '__main__':
     C = int(input())
     ans = []
-    for _ in range(C):
+    arrs = []
+    for i in range(C):
         input()
-        arr = [int(n) for n in input().split()]
-        ans.append(lis(arr))
+        arrs.append([int(n) for n in input().split()])
+        ans.append(get_real_lis(arrs[i]))
 
-    for n in ans:
-        print(n)
+
+    for i in range(len(ans)):
+        print(arrs[i], ans[i])
