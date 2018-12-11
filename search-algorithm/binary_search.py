645a003613c105e435d2d75b8e7a5055d756a02e
Sunghwan Park <shoark7@gmail.com>
2018-12-11 21:14:08 +0900

    Update binary search algorithm

--- search-algorithm/binary_search.py
+++ search-algorithm/binary_search.py
@@ -1,47 +1,22 @@
 """Binary search module
 
     Start Date: 2017/12/08
-    Last modified Date: 2017/12/08
+    Last modified Date: 2018/12/11
 
     github.com/shoark7
 """
-__version__ = '1.1.0'
+__version__ = '2.0.0'
 
 
-def binary_search(value, target, reverse=False, changed=False):
-    """Binary search in Python
-
-    This code is designed to be scalable, considering this kind of things,
-        1. ascending or descending can be chosen.
-        2. target list to be ransacked can be sorted during the search.
-        3. And more...
-
-    :input:
-        value:   Value to be searched
-        target:  List or something to be ransacked.
-        reverse: Whether search to be done in descending or not.
-        changed: Whether target to be changed during searching or not.
-
-    :return:
-        Index of the value in target.
-        If value is not in target, returns -1.
-    """
-    if value not in target:
-        return -1
-
-    if changed:
-        target.sort(reverse=reverse)
-        ordered = target
-    else:
-        ordered = sorted(target, reverse=reverse)
-
-    def search(start, end):
-        mid = (start + end) // 2
-        if value > ordered[mid]:
-            return search(mid, end)
-        elif value < ordered[mid]:
-            return search(start, mid)
+def binary_search(arr, v):
+    arr.sort()
+    def find(lo, hi):
+        mid = (lo + hi) // 2
+        if lo == hi:
+            return lo if arr[lo] == v else -1
+        elif arr[mid] < v:
+            return find(mid+1, hi)
         else:
-            return mid
+            return find(lo, mid)
 
-    return search(0, len(ordered)-1)
+    return find(0, len(arr)-1)
