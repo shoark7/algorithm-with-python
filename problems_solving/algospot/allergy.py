74090a459ca2a08ff67ab749e901b175e7437ef9
Sunghwan Park <shoark7@gmail.com>
2019-12-10 12:27:39 +0900

    Solve ALLERGY problem in algospot

--- problems_solving/algospot/allergy.py
+++ problems_solving/algospot/allergy.py
@@ -1,47 +1,18 @@
-"""Get the number of least menus that satisfy all the members in a party.
+"""Return minimum number of foods to satisfy all friends
 
-:input:
-2
-4 6
-cl bom dara minzy
-2 dara minzy
-2 cl minzy
-2 cl dara
-1 cl
-2 bom dara
-2 bom minzy
-10 7
-a b c d e f g h i j
-6 a c d h i j
-3 a d i
-7 a c f g h i j
-3 b d g
-5 b c f h i
-4 b e g j
-5 b c g h i
-
-:return:
-2
-3
-
-url : https://algospot.com/judge/problem/read/ALLERGY
-ID  : ALLERGY
+url: https://algospot.com/judge/problem/read/ALLERGY
+ID : ALLERGY
 """
-def min_menu_required(eater_of, i_can_eat):
-    """Return the least number of menus required to satisfy all our friends
+def minimum_foods(eaters, FRIEND):
+    FOOD = len(eaters)
+    best = float('inf')
+    edible = [0] * FRIEND
+    can_eat = [[] for _ in range(FRIEND)]
 
-    :input:
-        eater_of : A list of lists of friends who can eat those menus
-        i_can_eat: A list of lists of menus that each friend can eat
+    for i in range(len(eaters)):
+        for f in eaters[i]:
+            can_eat[f].append(i)
 
-    :return:
-        An integer of least number of menus required
-    """
-    N, M = len(i_can_eat), len(eater_of)
-    # edible: An temporary list of status of friends. Each element means number of menus a friend can eat
-    # with chosen menus
-    edible = [0 for _ in range(N)]
-    best = 99999
 
     def search(edible, chosen):
         nonlocal best
@@ -49,43 +20,42 @@ def min_menu_required(eater_of, i_can_eat):
             return
 
         first = 0
-        while first < N and edible[first] > 0:
+
+        while first < FRIEND and edible[first] > 0:
             first += 1
 
-        if first == N:
+        if first == FRIEND:
             best = chosen
             return
 
-        for m in range(len(i_can_eat[first])):
-            food = i_can_eat[first][m]
-            for j in range(len(eater_of[food])):
-                edible[eater_of[food][j]] += 1
+        for food in can_eat[first]:
+            for j in eaters[food]:
+                edible[j] += 1
+
             search(edible, chosen+1)
-            for j in range(len(eater_of[food])):
-                edible[eater_of[food][j]] -= 1
+
+            for j in eaters[food]:
+                edible[j] -= 1
 
     search(edible, 0)
     return best
 
 
 if __name__ == '__main__':
-    C = int(input())
+    T = int(input())
     ans = []
-    for _ in range(C):
-        N, M = (int(n) for n in input().split())
-        eater_of = []
-        i_can_eat = [[] for _ in range(N)]
-        names = [name for name in input().split()]
 
-        for _ in range(M):
-            eater_of.append([names.index(name) for name in input().split()[1:]])
+    for _ in range(T):
+        FRIEND, MEAL = (int(n) for n in input().strip().split())
+        friends = list(input().strip().split())
+        eaters = [list() for _ in range(MEAL)]
 
-        for i in range(len(eater_of)):
-            for f in eater_of[i]:
-                i_can_eat[f].append(i)
+        for food in eaters:
+            satisfies = list(input().strip().split())[1:]
+            for f in satisfies:
+                food.append(friends.index(f))
 
-        ans.append(min_menu_required(eater_of, i_can_eat))
-
+        ans.append(minimum_foods(eaters, FRIEND))
 
     for n in ans:
         print(n)
