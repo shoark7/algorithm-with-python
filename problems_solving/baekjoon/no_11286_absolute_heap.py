046d2e5db1123592c9b88758f714e87f9721c742
Sunghwan Park <shoark7@gmail.com>
2020-01-07 09:07:08 +0900

    Solve Baekjoon No.11286 Absolute heap

---
+++ problems_solving/baekjoon/no_11286_absolute_heap.py
@@ -0,0 +1,23 @@
+from heapq import heappop, heappush
+import sys
+
+input = sys.stdin.readline
+
+
+POPOUT = 0
+
+
+if __name__ == '__main__':
+    heap = []
+    N = int(input())
+
+    for _ in range(N):
+        n = int(input())
+        if n != POPOUT:
+            heappush(heap, (abs(n), n))
+        else:
+            if heap:
+                _, v = heappop(heap)
+                print(v)
+            else:
+                print(0)
